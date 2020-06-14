4846bbc5ecc8eae6ad1d4294274e8f07d3dc1e78
chiuannica <chiuannica@gmail.com>
2020-06-14 16:38:31 -0400

    hope the thing is still there lol

--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ viewEngine(app);
 //use body-parser to post data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/public", express.static(__dirname + "/public"));
 
 // init all web routes
 initWebRoute(app);
